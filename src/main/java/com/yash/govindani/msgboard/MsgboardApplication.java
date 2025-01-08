2afdca3006f54e27f4e1deab4945fb43ac9a6074
YashGovindani <yash.govindani@juspay.in>
2025-01-08 17:58:37 +0530

    boiler plate from Spring Initializr

---
+++ src/main/java/com/yash/govindani/msgboard/MsgboardApplication.java
@@ -0,0 +1,13 @@
+package com.yash.govindani.msgboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgboardApplication.class, args);
+	}
+
+}
