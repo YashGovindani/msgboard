c873e9d15acb93d2646ad813d3f47f92c0b3553c
YashGovindani <yash.govindani@juspay.in>
2025-01-25 00:05:35 +0530

    creating file on table creation

--- src/main/java/com/yash/govindani/msgboard/services/Administration.java
+++ src/main/java/com/yash/govindani/msgboard/services/Administration.java
@@ -8,6 +8,8 @@ import com.yash.govindani.msgboard.beans.*;
 import com.yash.govindani.msgboard.dao.*;
 import com.yash.govindani.msgboard.dto.*;
 import com.yash.govindani.msgboard.utils.*;
+import java.io.*;
+import com.google.gson.*;
 
 @Controller
 public class Administration {
@@ -21,16 +23,23 @@ public class Administration {
     @PostMapping("/install")
     public String installMessageBoard(InstallationBean installationBean) {
         try {
-        DatabaseUtility.createTables(installationBean.getDriver(), installationBean.getConnectionString(), installationBean.getUsername(), installationBean.getPassword());
-        } catch (DAOException exception) {
-            System.out.println("Unable to create tables"); // add to logs
-            return "InstallationFailed";
-        }
-        DAOConnection.setDriver(installationBean.getDriver());
-        DAOConnection.setConnectionString(installationBean.getConnectionString());
-        DAOConnection.setUsername(installationBean.getUsername());
-        DAOConnection.setPassword(installationBean.getPassword());
-        try {
+            DatabaseUtility.createTables(installationBean.getDriver(), installationBean.getConnectionString(), installationBean.getUsername(), installationBean.getPassword());
+            DAOConnection.setDriver(installationBean.getDriver());
+            DAOConnection.setConnectionString(installationBean.getConnectionString());
+            DAOConnection.setUsername(installationBean.getUsername());
+            DAOConnection.setPassword(installationBean.getPassword());
+            databaseBean.setDriver(installationBean.getDriver());
+            databaseBean.setConnectionString(installationBean.getConnectionString());
+            databaseBean.setUsername(installationBean.getUsername());
+            databaseBean.setPassword(installationBean.getPassword());
+            Gson gson = new Gson();
+            String jsonString = gson.toJson(databaseBean);
+            File file = new File("conf");
+            file.mkdir();
+            file = new File("conf" + File.separator + "db.json");
+            file.createNewFile();
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write(jsonString);
             Administrator administrator = new Administrator();
             administrator.setUsername(installationBean.getUsername());
             String passwordKey = EncryptionUtility.getKey();
@@ -40,6 +49,9 @@ public class Administration {
             AdministratorDAO.add(administrator);
             return "InstallationSuccessful";
         } catch (DAOException exception) {
+            System.out.println(exception.getMessage()); //  add to logs
+            return "InstallationFailed";
+        } catch (IOException exception) {
             System.out.println(exception.getMessage()); //  add to logs
             return "InstallationFailed";
         } catch (Exception exception) {
